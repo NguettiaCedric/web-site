07174af75b823969da7bb0506676866a10a9f131
N'guettia Cedric <iko.drico@gmail.com>
2024-02-29 08:36:31 +0000

    commit

--- components/sections/Services1.js
+++ components/sections/Services1.js
@@ -15,9 +15,10 @@ export default function Services1() {
                                 {/* <span className="sub-title">our services</span> */}
                                 <h2>Découvrez les services<br />nous proposons</h2>
                             </div>
-                            {/* <div className="col-lg-5">
-                                <div className="text">There are many variations of passages of available but majority alteration in some form, by humou or randomised words.</div>
-                            </div> */}
+                            <div className="col-lg-5">
+                                {/* <div className="text">There are many variations of passages of available but majority alteration in some form, by humou or randomised words.</div> */}
+                                <a class="theme-btn btn-style-one" href="" ><span class="btn-title">Demande de demo</span></a>
+                            </div>
                         </div>
                     </div>
                     <div className="row">
