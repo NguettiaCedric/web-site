d66673a4eda682f8001f04bcc946512089b51184
Aimé LINGUE <lingueaime@gmail.com>
2024-03-05 10:17:09 +0100

    ajout de favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d926931220225e3a"; }
+/******/ 		__webpack_require__.h = function() { return "fa9c35006835587d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
