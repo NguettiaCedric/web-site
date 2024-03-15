47d49807f757d929a99964aadac2d2b122838eec
N'guettia Cedric <iko.drico@gmail.com>
2024-03-15 14:16:39 +0000

    14h06

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b47f14dac5086022"; }
+/******/ 		__webpack_require__.h = function() { return "6f122211f85d1898"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -11,7 +11,7 @@ const PageHead = ({ headTitle }) => {
                     href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"
                 />
-                <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.1.0/uicons-bold-straight/css/uicons-bold-straight.css'></link>
+                {/* <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.1.0/uicons-bold-straight/css/uicons-bold-straight.css'></link> */}
                 
             </Head>
         </>
